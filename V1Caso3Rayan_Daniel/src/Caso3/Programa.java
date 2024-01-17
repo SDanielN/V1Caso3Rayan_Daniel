d9b2b3b62b1c3d5f55f7ec1f60440f77efb1beb6
daniel <daniel@1DAM-10>
2024-01-17 12:33:45 +0100

    Commit del volumen de un cubo

--- V1Caso3Rayan_Daniel/src/Caso3/Programa.java
+++ V1Caso3Rayan_Daniel/src/Caso3/Programa.java
@@ -35,20 +35,26 @@ public class Programa {
 		double alturaCIlindro = entrada.nextInt();
 
 		System.out.println("El area del cilindro es de " + calcularArea(radioCilindro, alturaCIlindro));
-		
-		  // Solicitar al usuario que ingrese la base del rectángulo
-        System.out.print("Ingrese la base del rectángulo: ");
-        double baseRec = entrada.nextDouble();
 
-        // Solicitar al usuario que ingrese la altura del rectángulo
-        System.out.print("Ingrese la altura del rectángulo: ");
-        double alturaRec = entrada.nextDouble();
+		// Solicitar al usuario que ingrese la base del rectángulo
+		System.out.print("Ingrese la base del rectángulo: ");
+		double baseRec = entrada.nextDouble();
 
-        // Calcular el área del rectángulo
-        double areaRec = calcularAreaRectangulo(baseRec, alturaRec);
+		// Solicitar al usuario que ingrese la altura del rectángulo
+		System.out.print("Ingrese la altura del rectángulo: ");
+		double alturaRec = entrada.nextDouble();
 
-        // Mostrar el resultado
-        System.out.println("Área del rectángulo: " + area);
+		// Calcular el área del rectángulo
+		double areaRec = calcularAreaRectangulo(baseRec, alturaRec);
+
+		// Mostrar el resultado
+		System.out.println("Área del rectángulo: " + area);
+
+		// Pedimos los datos del cubo
+		System.out.println("Escribe el lado del cubo: ");
+		double ladoCubo = entrada.nextInt();
+
+		System.out.println("El area del cubo es de " + calcularAreaCubo(ladoCubo));
 
 	}
 
@@ -74,8 +80,16 @@ public class Programa {
 		double area = 2 * 3.14 * radio * (radio + altura);
 		return area;
 	}
-	 // Función para calcular el área del rectángulo
-    public static double calcularAreaRectangulo(double baseRec, double alturaRec) {
-        return baseRec * alturaRec;
-    }
+
+	// Función para calcular el área del rectángulo
+	public static double calcularAreaRectangulo(double baseRec, double alturaRec) {
+		return baseRec * alturaRec;
+	}
+
+	// Calculamos el área del cubo - Alumno2
+	public static double calcularAreaCubo(double lado) {
+		double areaCara = lado * lado;
+		double areaCubo = areaCara * 6;
+		return areaCubo;
+	}
 }
