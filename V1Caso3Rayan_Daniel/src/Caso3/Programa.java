acef61a907328a95a26960aae6e46cf42bb7765a
daniel <daniel@1DAM-10>
2024-01-17 12:06:58 +0100

    Commit 2/area cilindro

--- V1Caso3Rayan_Daniel/src/Caso3/Programa.java
+++ V1Caso3Rayan_Daniel/src/Caso3/Programa.java
@@ -15,18 +15,26 @@ public class Programa {
 		double alturaTriangulo = entrada.nextInt();
 
 		System.out.println("El area del triangulo es de " + calcularArea(baseTriangulo, alturaTriangulo));
-	     // Solicitar al usuario que ingrese el radio de la circunferencia
-        System.out.print("Ingrese el radio de la circunferencia: ");
-        double radio = entrada.nextDouble();
+		// Solicitar al usuario que ingrese el radio de la circunferencia
+		System.out.print("Ingrese el radio de la circunferencia: ");
+		double radio = entrada.nextDouble();
 
-        // Calcular el área y el perímetro
-        double area = calcularArea(radio);
-        double perimetro = calcularPerimetro(radio);
+		// Calcular el área y el perímetro
+		double area = calcularArea(radio);
+		double perimetro = calcularPerimetro(radio);
 
-        // Mostrar los resultados
-        System.out.println("Área de la circunferencia: " + area);
-        System.out.println("Perímetro de la circunferencia: " + perimetro);
+		// Mostrar los resultados
+		System.out.println("Área de la circunferencia: " + area);
+		System.out.println("Perímetro de la circunferencia: " + perimetro);
 
+		// Pedimos los datos del cilindro
+		System.out.println("Escribe el radio del cilindro: ");
+		double radioCilindro = entrada.nextInt();
+
+		System.out.println("Escribe la altura del cilindro: ");
+		double alturaCIlindro = entrada.nextInt();
+
+		System.out.println("El area del cilindro es de " + calcularArea(radioCilindro, alturaCIlindro));
 
 	}
 
@@ -36,13 +44,20 @@ public class Programa {
 		double area = lado1 * lado2 / 2;
 		return area;
 	}
-	  // Función para calcular el área de la circunferencia
-    public static double calcularArea(double radio) {
-        return Math.PI * Math.pow(radio, 2);
-    }
 
-    // Función para calcular el perímetro de la circunferencia
-    public static double calcularPerimetro(double radio) {
-        return 2 * Math.PI * radio;
-    }
+	// Función para calcular el área de la circunferencia
+	public static double calcularArea(double radio) {
+		return Math.PI * Math.pow(radio, 2);
+	}
+
+	// Función para calcular el perímetro de la circunferencia
+	public static double calcularPerimetro(double radio) {
+		return 2 * Math.PI * radio;
+	}
+
+	// Calculamos el área del cilindro
+	public static double calcularAreaCilindro(double radio, double altura) {
+		double area = 2 * 3.14 * radio * (radio + altura);
+		return area;
+	}
 }
